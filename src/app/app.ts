c66f0db045182c85863eef55906ba10db70242d5
Artur Junior <arturcmjr@gmail.com>
2025-10-10 23:08:04 -0300

    header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { NightSkyComponent } from './layout/night-sky/night-sky';
+import { Header } from './layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [ NightSkyComponent],
+  imports: [ NightSkyComponent, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/layout/header/header.ts
@@ -0,0 +1,49 @@
+import {
+  Component,
+  signal,
+  inject,
+  DOCUMENT,
+  OnInit,
+  OnDestroy,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'glass',
+    '[class.hidden]': 'hidden()',
+  },
+})
+export class Header implements OnInit, OnDestroy {
+  private readonly document = inject(DOCUMENT);
+  protected readonly hidden = signal(false);
+
+  private lastScroll = 0;
+  private scrollListener = this.onScroll.bind(this);
+
+  ngOnInit() {
+    this.document.body?.addEventListener('scroll', this.scrollListener, { passive: true });
+  }
+
+  ngOnDestroy() {
+    this.document.body?.removeEventListener('scroll', this.scrollListener);
+  }
+
+  private onScroll() {
+    const currentScroll =
+      this.document.documentElement.scrollTop || this.document.body.scrollTop || 0;
+
+    if (currentScroll > this.lastScroll) {
+      this.hidden.set(true);
+    } else {
+      this.hidden.set(false);
+    }
+
+    this.lastScroll = currentScroll <= 0 ? 0 : currentScroll;
+  }
+}
